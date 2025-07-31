2ac02044265d318f968addae2d02140131f71042
WretchedPenguin <wretchedpenguin@proton.me>
2025-07-31 14:29:38 +0200

    Use file-scoped namespaces

--- ScarecrowHighlighter/HighlightedDrawer.cs
+++ ScarecrowHighlighter/HighlightedDrawer.cs
@@ -3,71 +3,70 @@ using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI.Events;
 using StardewValley;
 
-namespace ScarecrowHighlighter
+namespace ScarecrowHighlighter;
+
+public class HighlightedDrawer
 {
-    public class HighlightedDrawer
+    private readonly Dictionary<Vector2, int> _highlights = new();
+
+    public void Add(Vector2 location, int radius)
     {
-        private readonly Dictionary<Vector2, int> _highlights = new();
+        _highlights[location] = radius;
+    }
 
-        public void Add(Vector2 location, int radius)
+    public void Clear()
+    {
+        _highlights.Clear();
+    }
+
+    public void DrawHighlightedObjects(RenderedWorldEventArgs e)
+    {
+        foreach (var toDraw in _highlights)
         {
-            _highlights[location] = radius;
+            DrawObject(e.SpriteBatch, toDraw.Key, toDraw.Value);
         }
+    }
 
-        public void Clear()
+    private static void DrawObject(SpriteBatch spriteBatch, Vector2 tileLocation, int radius)
+    {
+        var locations = GetLocationsInRadius(tileLocation, radius);
+
+        const int cursorSize = 16;
+
+        foreach (var location in locations)
         {
-            _highlights.Clear();
-        }
-
-        public void DrawHighlightedObjects(RenderedWorldEventArgs e)
-        {
-            foreach (var toDraw in _highlights)
-            {
-                DrawObject(e.SpriteBatch, toDraw.Key, toDraw.Value);
-            }
-        }
-
-        private static void DrawObject(SpriteBatch spriteBatch, Vector2 tileLocation, int radius)
-        {
-            var locations = GetLocationsInRadius(tileLocation, radius);
-
-            const int cursorSize = 16;
-
-            foreach (var location in locations)
-            {
-                spriteBatch.Draw(
-                    Game1.mouseCursors,
-                    TileToScreen(location),
-                    new Rectangle(194, 388, cursorSize, cursorSize),
-                    Color.White,
-                    0,
-                    Vector2.Zero,
-                    new Vector2(Game1.tileSize / (float) cursorSize),
-                    SpriteEffects.None,
-                    0
-                );
-            }
-        }
-
-        private static Vector2 TileToScreen(Vector2 location)
-        {
-            return new Vector2(
-                location.X * Game1.tileSize - Game1.viewport.X,
-                location.Y * Game1.tileSize - Game1.viewport.Y
+            spriteBatch.Draw(
+                Game1.mouseCursors,
+                TileToScreen(location),
+                new Rectangle(194, 388, cursorSize, cursorSize),
+                Color.White,
+                0,
+                Vector2.Zero,
+                new Vector2(Game1.tileSize / (float) cursorSize),
+                SpriteEffects.None,
+                0
             );
         }
+    }
 
-        private static IEnumerable<Vector2> GetLocationsInRadius(Vector2 location, int radius)
+    private static Vector2 TileToScreen(Vector2 location)
+    {
+        return new Vector2(
+            location.X * Game1.tileSize - Game1.viewport.X,
+            location.Y * Game1.tileSize - Game1.viewport.Y
+        );
+    }
+
+    private static IEnumerable<Vector2> GetLocationsInRadius(Vector2 location, int radius)
+    {
+        for (float x = -radius; x < radius; x++)
         {
-            for (float x = -radius; x < radius; x++)
+            for (var y = -radius; y < radius; y++)
             {
-                for (var y = -radius; y < radius; y++)
+                var tileLocation = new Vector2(location.X + x, location.Y + y);
+                if (Vector2.Distance(location, tileLocation) < radius)
                 {
-                    var tileLocation = new Vector2(location.X + x, location.Y + y);
-                    if (Vector2.Distance(location, tileLocation) < radius)
-                    {
-                        yield return tileLocation;
-                    }
+                    yield return tileLocation;
                 }
             }
         }
--- ScarecrowHighlighter/ScarecrowHighlighter.cs
+++ ScarecrowHighlighter/ScarecrowHighlighter.cs
@@ -3,177 +3,176 @@ using StardewModdingAPI.Events;
 using StardewValley;
 using Object = StardewValley.Object;
 
-namespace ScarecrowHighlighter
+namespace ScarecrowHighlighter;
+
+public class ScarecrowHighlighterMod : Mod
 {
-    public class ScarecrowHighlighterMod : Mod
+    private bool _hovering;
+    private bool _pressed;
+
+    private ModConfig _config = null!;
+
+    private readonly HighlightedDrawer _drawer = new();
+    private readonly Dictionary<Object, int> _itemsToHighlight = new();
+
+    private readonly Dictionary<string, int> _radiusByQualifiedId = new();
+
+    public override void Entry(IModHelper helper)
     {
-        private bool _hovering;
-        private bool _pressed;
+        I18n.Init(helper.Translation);
 
-        private ModConfig _config = null!;
+        _config = Helper.ReadConfig<ModConfig>();
 
-        private readonly HighlightedDrawer _drawer = new();
-        private readonly Dictionary<Object, int> _itemsToHighlight = new();
+        helper.Events.GameLoop.GameLaunched += BuildHighlightingList;
+        helper.Events.GameLoop.GameLaunched += RegisterModConfigMenu;
 
-        private readonly Dictionary<string, int> _radiusByQualifiedId = new();
+        helper.Events.Input.CursorMoved += InputOnCursorMoved;
+        helper.Events.Display.RenderedWorld += DisplayOnRenderedWorld;
+        helper.Events.World.ObjectListChanged += WorldOnObjectListChanged;
+        helper.Events.Player.Warped += PlayerOnWarped;
+        helper.Events.Input.ButtonPressed += CheckButtonPressed;
+    }
 
-        public override void Entry(IModHelper helper)
+
+    /// <summary>
+    /// Builds a list of objects to highlight based on the name of registered items.
+    /// The radius from the scarecrow is taken from the official method <see cref="Object.GetRadiusForScarecrow"/>
+    /// </summary>
+    private void BuildHighlightingList(object? sender, GameLaunchedEventArgs e)
+    {
+        foreach (var itemType in ItemRegistry.ItemTypes.Where(x => x.Identifier == "(BC)"))
         {
-            I18n.Init(helper.Translation);
+            foreach (var id in itemType.GetAllIds())
+            {
+                var data = itemType.GetData(id);
 
-            _config = Helper.ReadConfig<ModConfig>();
+                if (!data.InternalName.Contains("arecrow", StringComparison.OrdinalIgnoreCase)) continue;
 
-            helper.Events.GameLoop.GameLaunched += BuildHighlightingList;
-            helper.Events.GameLoop.GameLaunched += RegisterModConfigMenu;
+                var radius = data.InternalName.Contains("deluxe", StringComparison.OrdinalIgnoreCase) ? 17 : 9;
+                _radiusByQualifiedId.Add(data.QualifiedItemId, radius);
+            }
+        }
+    }
 
-            helper.Events.Input.CursorMoved += InputOnCursorMoved;
-            helper.Events.Display.RenderedWorld += DisplayOnRenderedWorld;
-            helper.Events.World.ObjectListChanged += WorldOnObjectListChanged;
-            helper.Events.Player.Warped += PlayerOnWarped;
-            helper.Events.Input.ButtonPressed += CheckButtonPressed;
+    private void RegisterModConfigMenu(object? sender, GameLaunchedEventArgs e)
+    {
+        // get Generic Mod Config Menu's API (if it's installed)
+        var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+        if (configMenu is null)
+            return;
+
+        configMenu.Register(
+            mod: ModManifest,
+            reset: () => _config = new ModConfig(),
+            save: () => Helper.WriteConfig(_config)
+        );
+
+        configMenu.AddBoolOption(
+            mod: ModManifest,
+            name: I18n.Config_HighlightOnHold_Name,
+            tooltip: I18n.Config_HighlightOnHold_Tooltip,
+            getValue: () => _config.HighlightOnHold,
+            setValue: value => _config.HighlightOnHold = value
+        );
+
+        configMenu.AddBoolOption(
+            mod: ModManifest,
+            name: I18n.Config_HighlightOnHover_Name,
+            tooltip: I18n.Config_HighlightOnHover_Tooltip,
+            getValue: () => _config.HighlightOnHovered,
+            setValue: value => _config.HighlightOnHovered = value
+        );
+
+        configMenu.AddKeybind(
+            mod: ModManifest,
+            name: I18n.Config_ToggleHighlight_Name,
+            getValue: () => _config.ToggleHighlightButton,
+            setValue: value => _config.ToggleHighlightButton = value
+        );
+    }
+
+    private void DisplayOnRenderedWorld(object? sender, RenderedWorldEventArgs e)
+    {
+        _drawer.Clear();
+
+        var holding = CheckHoldingHighlight();
+        if (!_hovering && !holding && !_pressed)
+            return;
+
+        foreach (var pair in _itemsToHighlight)
+        {
+            _drawer.Add(pair.Key.TileLocation, pair.Value);
         }
 
+        _drawer.DrawHighlightedObjects(e);
+    }
 
-        /// <summary>
-        /// Builds a list of objects to highlight based on the name of registered items.
-        /// The radius from the scarecrow is taken from the official method <see cref="Object.GetRadiusForScarecrow"/>
-        /// </summary>
-        private void BuildHighlightingList(object? sender, GameLaunchedEventArgs e)
+    private void CheckButtonPressed(object? sender, ButtonPressedEventArgs e)
+    {
+        if (e.Button != _config.ToggleHighlightButton) return;
+
+        _pressed = !_pressed;
+    }
+
+    private bool CheckHoldingHighlight()
+    {
+        if (!Context.IsWorldReady) return false;
+        if (Game1.player.CurrentItem == null) return false;
+        if (!_config.HighlightOnHold) return false;
+
+        var holding = _radiusByQualifiedId.TryGetValue(Game1.player.CurrentItem.QualifiedItemId, out var radius);
+
+        if (holding)
         {
-            foreach (var itemType in ItemRegistry.ItemTypes.Where(x => x.Identifier == "(BC)"))
+            _drawer.Add(Game1.currentCursorTile, radius);
+        }
+
+        return holding;
+    }
+
+    private void PlayerOnWarped(object? sender, WarpedEventArgs e)
+    {
+        foreach (var obj in e.NewLocation.Objects.Values)
+        {
+            if (_radiusByQualifiedId.TryGetValue(obj.QualifiedItemId, out var radius))
             {
-                foreach (var id in itemType.GetAllIds())
-                {
-                    var data = itemType.GetData(id);
+                _itemsToHighlight.Add(obj, radius);
+            }
+        }
+    }
 
-                    if (!data.InternalName.Contains("arecrow", StringComparison.OrdinalIgnoreCase)) continue;
+    private void WorldOnObjectListChanged(object? sender, ObjectListChangedEventArgs e)
+    {
+        if (!e.IsCurrentLocation) return;
 
-                    var radius = data.InternalName.Contains("deluxe", StringComparison.OrdinalIgnoreCase) ? 17 : 9;
-                    _radiusByQualifiedId.Add(data.QualifiedItemId, radius);
-                }
+        foreach (var added in e.Added)
+        {
+            if (_radiusByQualifiedId.TryGetValue(added.Value.QualifiedItemId, out var radius))
+            {
+                _itemsToHighlight.Add(added.Value, radius);
             }
         }
 
-        private void RegisterModConfigMenu(object? sender, GameLaunchedEventArgs e)
+        foreach (var removed in e.Removed)
         {
-            // get Generic Mod Config Menu's API (if it's installed)
-            var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
-            if (configMenu is null)
-                return;
-
-            configMenu.Register(
-                mod: ModManifest,
-                reset: () => _config = new ModConfig(),
-                save: () => Helper.WriteConfig(_config)
-            );
-
-            configMenu.AddBoolOption(
-                mod: ModManifest,
-                name: I18n.Config_HighlightOnHold_Name,
-                tooltip: I18n.Config_HighlightOnHold_Tooltip,
-                getValue: () => _config.HighlightOnHold,
-                setValue: value => _config.HighlightOnHold = value
-            );
-
-            configMenu.AddBoolOption(
-                mod: ModManifest,
-                name: I18n.Config_HighlightOnHover_Name,
-                tooltip: I18n.Config_HighlightOnHover_Tooltip,
-                getValue: () => _config.HighlightOnHovered,
-                setValue: value => _config.HighlightOnHovered = value
-            );
-
-            configMenu.AddKeybind(
-                mod: ModManifest,
-                name: I18n.Config_ToggleHighlight_Name,
-                getValue: () => _config.ToggleHighlightButton,
-                setValue: value => _config.ToggleHighlightButton = value
-            );
-        }
-
-        private void DisplayOnRenderedWorld(object? sender, RenderedWorldEventArgs e)
-        {
-            _drawer.Clear();
-
-            var holding = CheckHoldingHighlight();
-            if (!_hovering && !holding && !_pressed)
-                return;
-
-            foreach (var pair in _itemsToHighlight)
+            if (_radiusByQualifiedId.ContainsKey(removed.Value.QualifiedItemId))
             {
-                _drawer.Add(pair.Key.TileLocation, pair.Value);
-            }
-
-            _drawer.DrawHighlightedObjects(e);
-        }
-
-        private void CheckButtonPressed(object? sender, ButtonPressedEventArgs e)
-        {
-            if (e.Button != _config.ToggleHighlightButton) return;
-
-            _pressed = !_pressed;
-        }
-
-        private bool CheckHoldingHighlight()
-        {
-            if (!Context.IsWorldReady) return false;
-            if (Game1.player.CurrentItem == null) return false;
-            if (!_config.HighlightOnHold) return false;
-
-            var holding = _radiusByQualifiedId.TryGetValue(Game1.player.CurrentItem.QualifiedItemId, out var radius);
-
-            if (holding)
-            {
-                _drawer.Add(Game1.currentCursorTile, radius);
-            }
-
-            return holding;
-        }
-
-        private void PlayerOnWarped(object? sender, WarpedEventArgs e)
-        {
-            foreach (var obj in e.NewLocation.Objects.Values)
-            {
-                if (_radiusByQualifiedId.TryGetValue(obj.QualifiedItemId, out var radius))
-                {
-                    _itemsToHighlight.Add(obj, radius);
-                }
+                _itemsToHighlight.Remove(removed.Value);
             }
         }
+    }
 
-        private void WorldOnObjectListChanged(object? sender, ObjectListChangedEventArgs e)
-        {
-            if (!e.IsCurrentLocation) return;
+    private void InputOnCursorMoved(object? sender, CursorMovedEventArgs e)
+    {
+        _hovering = false;
+        if (!Context.IsWorldReady) return;
+        if (!_config.HighlightOnHovered) return;
 
-            foreach (var added in e.Added)
-            {
-                if (_radiusByQualifiedId.TryGetValue(added.Value.QualifiedItemId, out var radius))
-                {
-                    _itemsToHighlight.Add(added.Value, radius);
-                }
-            }
+        var tile = e.NewPosition.Tile;
+        var hovered = Game1.currentLocation.getObjectAtTile((int) tile.X, (int) tile.Y);
+        if (hovered == null) return;
+        if (!_radiusByQualifiedId.ContainsKey(hovered.QualifiedItemId)) return;
 
-            foreach (var removed in e.Removed)
-            {
-                if (_radiusByQualifiedId.ContainsKey(removed.Value.QualifiedItemId))
-                {
-                    _itemsToHighlight.Remove(removed.Value);
-                }
-            }
-        }
-
-        private void InputOnCursorMoved(object? sender, CursorMovedEventArgs e)
-        {
-            _hovering = false;
-            if (!Context.IsWorldReady) return;
-            if (!_config.HighlightOnHovered) return;
-
-            var tile = e.NewPosition.Tile;
-            var hovered = Game1.currentLocation.getObjectAtTile((int) tile.X, (int) tile.Y);
-            if (hovered == null) return;
-            if (!_radiusByQualifiedId.ContainsKey(hovered.QualifiedItemId)) return;
-
-            _hovering = true;
-        }
+        _hovering = true;
     }
 }
